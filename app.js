137b104ef4af7b3646a1c25fd1e01d543acb0c28
Robert Perez <robgaray9111@yahoo.com>
2020-05-28 10:36:55 -0700

    Added DB connection to app

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const connectDB = require("./config/db");
 const HOST = "127.0.0.1";
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
